1c46fba647edf55fe2f6c0046b330d0baf8cd6bf
LaZY <18846171630@163.com>
2019-04-16 13:25:14 +0800

    add-104.二叉树深度-code

---
+++ algorithm/src/alg104/Solution.java
@@ -0,0 +1,25 @@
+package alg104;
+
+/**
+ * 二叉树深度
+ * @author LaZY(李志一)
+ * @create 2019-04-16 11:07
+ */
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+}
