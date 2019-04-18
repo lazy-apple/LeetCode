807b37ba51bf4293ce2e6e7d9a2cd6dc81ceeff2
LaZY <18846171630@163.com>
2019-04-18 10:39:59 +0800

    add-502.ipo-code

---
+++ algorithm/src/alg502/Solution.java
@@ -0,0 +1,60 @@
+package alg502;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author LaZY(李志一)
+ * @create 2019-04-18 10:38
+ */
+public class Solution {
+    public static class Node {
+        public int p;
+        public int c;
+
+        public Node(int p, int c) {
+            this.p = p;
+            this.c = c;
+        }
+    }
+    public static class MinCostComparator implements Comparator<Node> {
+
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o1.c - o2.c;
+        }
+
+    }
+
+    public static class MaxProfitComparator implements Comparator<Node> {
+
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o2.p - o1.p;
+        }
+
+    }
+
+    public static int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        Node[] nodes = new Node[Profits.length];
+        for (int i = 0; i < Profits.length; i++) {
+            nodes[i] = new Node(Profits[i], Capital[i]);
+        }
+
+        PriorityQueue<Node> minCostQ = new PriorityQueue<>(new MinCostComparator());
+        PriorityQueue<Node> maxProfitQ = new PriorityQueue<>(new MaxProfitComparator());
+        for (int i = 0; i < nodes.length; i++) {
+            minCostQ.add(nodes[i]);
+        }
+        for (int i = 0; i < k; i++) {
+            while (!minCostQ.isEmpty() && minCostQ.peek().c <= W) {
+                maxProfitQ.add(minCostQ.poll());
+            }
+            if (maxProfitQ.isEmpty()) {
+                return W;
+            }
+            W += maxProfitQ.poll().p;
+        }
+        return W;
+    }
+}
